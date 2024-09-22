d1ff8577a1456c72751d001422ca148dd1f13808
FairozSheikh <fairoz.marvelminds@gmail.com>
2024-09-22 22:07:10 +0530

    Pythagoreus_Theorum✅

---
+++ 40_Pythagorean_Theorum.py
@@ -0,0 +1,5 @@
+import math
+side1=float(input("Enter side one triangle : "))
+side2=float(input("Enter side two triangle : "))
+hypotnese=math.sqrt((side1**2)+(side2**2))
+print("Hypotnese is equal to : "+str(hypotnese))
