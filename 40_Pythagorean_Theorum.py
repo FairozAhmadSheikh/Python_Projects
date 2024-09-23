4f2fadc50fe6a7dfd402a8e6324a0e742fdea6d7
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2024-09-23 16:21:48 +0530

    done

--- 40_Pythagorean_Theorum.py
+++ 40_Pythagorean_Theorum.py
@@ -1,5 +1,5 @@
 import math
 side1=float(input("Enter side one triangle : "))
 side2=float(input("Enter side two triangle : "))
-hypotnese=math.sqrt((side1**2)+(side2**2))
+hypotnese=math.sqrt((side1**)+(side2**))
 print("Hypotnese is equal to : "+str(hypotnese))
